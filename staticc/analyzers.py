273d23f87d18f5405d94ddc0b0860624c0e9d7f3
Batuhan Taskaya <isidentical@gmail.com>
2022-10-30 03:54:59 +0300

    feat: add the initial `Analyzer` template

---
+++ staticc/analyzers.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import ast
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+ResultT = TypeVar("ResultT")
+ExpectedNodeT = TypeVar("ExpectedNodeT", bound=ast.AST)
+
+
+class _GenericAnalyzer(Generic[ExpectedNodeT, ResultT]):
+    def analyze(self, node: ExpectedNodeT) -> ResultT:
+        ...
+
+
+@dataclass
+class Analyzer(_GenericAnalyzer[ast.AST, ResultT]):
+    """A generic analyzer that can be used for any node in the same
+    tree (state)."""
+
+    def analyze(self, node: ast.AST) -> ResultT:
+        raise NotImplementedError
